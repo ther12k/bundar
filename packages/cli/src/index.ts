b1cdd2e5f8062fa49e8e5d740f2bce2eccb2c3e0
Rizky Zulkarnaen <rizkyz@gmail.com>
2026-08-23 16:16:04 +0700

    style: prettier cli machine contract

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,10 +3,6 @@
  */
 export { BUNDAR_VERSION, parseArgs, registerCommand, runCli } from "./cli";
 
-export type {
-  CommandContext,
-  CommandDefinition,
-  CommandHandler,
-} from "./cli";
+export type { CommandContext, CommandDefinition, CommandHandler } from "./cli";
 export type { CliEnvelopeV1, ExitCode } from "./machine";
 export { buildEnvelope, EXIT, printEnvelope, stripAnsi } from "./machine";
